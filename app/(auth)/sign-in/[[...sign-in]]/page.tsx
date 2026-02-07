5b80ef2a9c455906a99536a69156d0466418f10f
Mohamed <elkhamlichim099@gmail.com>
2026-02-07 22:19:56 +0100

    add background

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-6">
+    <div className="min-h-screen flex items-center justify-center p-6 bg-[url('/bg.png')] bg-cover bg-center">
       <SignIn
         appearance={{
           theme: "simple",
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-6">
+    <div className="min-h-screen flex items-center justify-center p-6 bg-[url('/bg.png')] bg-cover bg-center">
       <SignUp
         appearance={{
           theme: "simple",
